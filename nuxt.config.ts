f72f62e1f30ad14673ba4e01f14b4fae25640261
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-11-29 23:47:27 +0100

    Installing tailwind and improving card

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  buildModules: [
+    '@nuxt/postcss8',
+  ],
+  tailwindcss: {
+    // add '~tailwind.config` alias
+    exposeConfig: true
+  },
+  css: [
+    '@/assets/css/main.css',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+
+
 
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,49 @@
 <script setup>
-const { data } = await useFetch('/api/tickers?limit=10')
+const { data } = await useFetch("/api/tickers?limit=10");
 </script>
 
 <template>
-  <main>
-    <h1>Index Page</h1>
-
-    <table border="1 px solid">
-      <thead>
-      <tr>
-        <th>Name</th>
-        <th>Symbol</th>
-        <th>Price</th>
-        <th>Details</th>
-      </tr>
-      </thead>
-      <tr v-for="currency in data.data" :key="data.data.id">
-        <td>{{ currency.name }}</td>
-        <td>{{ currency.symbol }}</td>
-        <td>{{ currency.price_usd }}</td>
-        <td>
-          <NuxtLink :to="'/currency/' + currency.id">{{ currency.id }}</NuxtLink>
-        </td>
-      </tr>
-    </table>
+  <main class="antialiased  text-slate-700 mx-2">
+    <div
+      class="
+        max-w-lg
+        mx-auto
+        my-10
+        bg-white
+        p-8
+        rounded-xl
+        shadow shadow-slate-300
+      "
+    >
+      <div class="flex flex-col justify-between items-center">
+        <div>
+          <h2 class="text-3xl font-bold underline">Index Page</h2>
+        </div>
+        <div class="flex justify-between items-center border-b border-slate-200 py-3 px-2 border-l-4  border-l-transparent">
+          <table border="1 px solid">
+            <thead>
+              <tr class="inline-flex items-center space-x-2">
+                <th>Name</th>
+                <th>Symbol</th>
+                <th>Price</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tr v-for="currency in data.data" :key="data.data.id" class="flex justify-between items-center border-b border-slate-200 py-3 px-1 border-l-4  border-l-transparent" >
+              <td>{{ currency.name }}</td>
+              <td>{{ currency.symbol }}</td>
+              <td>{{ currency.price_usd }}</td>
+              <td>
+                <NuxtLink :to="'/currency/' + currency.id">{{
+                  currency.id
+                }}</NuxtLink>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+<style scoped>
+</style>
